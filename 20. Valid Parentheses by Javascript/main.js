6c60ad2c94c9da25869410957f8edc5fb098cf9d
Thina Karan <75832218+thinakaran123@users.noreply.github.com>
2025-06-05 16:59:12 -0400

    Update main.js

--- 20. Valid Parentheses by Javascript/main.js
+++ 20. Valid Parentheses by Javascript/main.js
@@ -1,1 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char); // Push opening brackets
+        } else {
+            // If stack is empty or top doesn't match
+            if (stack.length === 0 || stack[stack.length - 1] !== map[char]) {
+                return false;
+            }
+            stack.pop(); // Valid match, remove opening
+        }
+    }
+
+    // Final check: Stack should be empty
+    return stack.length === 0;
+};
 
