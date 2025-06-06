9a30cc38f0b56a83242f630e579b9937e1761fd5
Thina Karan <75832218+thinakaran123@users.noreply.github.com>
2025-06-06 09:57:05 -0400

    Update main.js

--- 21. Merge Two sorted Lists by Js/main.js
+++ 21. Merge Two sorted Lists by Js/main.js
@@ -1,1 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
 
+  //place for starting node
+  let placeforfirstnode=new ListNode(-1);
+
+  //this is current node
+  currentNode=placeforfirstnode;
+  
+while(list1!==null &&  list2!==null){
+   if(list1.val < list2.val){
+    currentNode.next=list1;
+    list1=list1.next
+   }else{
+    currentNode.next=list2;
+    list2=list2.next
+   }
+   currentNode=currentNode.next
+} 
+   currentNode.next=list1!==null?list1:list2;
+
+    return placeforfirstnode.next;
+};
